469ee992952aa52973089b7d6836c9596f8eeb77
Thanges D <thangeswaran@farshore.com>
2018-08-11 16:12:59 +0530

    Add code to display the logged in User name in Dashboard message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-               
+
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -14,11 +14,10 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! Welcome {{Auth::user()->name}}
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
-
